ef0c9e849f3e8947337077d4d91372c1c62bec41
T0ren4ik <100593889+T0ren4ik@users.noreply.github.com>
2023-04-08 10:58:29 +0300

    Added modules and classes to main file

--- lib/probabilityTheory.rb
+++ lib/probabilityTheory.rb
@@ -3,13 +3,11 @@
 require_relative "probabilityTheory/version"
 require_relative "probabilityTheory/features"
 require_relative "probabilityTheory/cart"
+require_relative "probabilityTheory/dice"
 
 include Features
 
 module ProbabilityTheory
   class Error < StandardError; end
   # Your code goes here...
-  def self.hello
-    'hello'
-  end
 end
